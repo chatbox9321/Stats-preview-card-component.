ed45dd7316ab12198ef60f1fc3d9823c11aeac62
chatbox9321 <85362220+chatbox9321@users.noreply.github.com>
2021-06-19 20:57:57 +0530

    Add files via upload
    Stats Preview.

---
+++ stats-preview-card-component-main/index.js
@@ -0,0 +1,6 @@
+document.querySelector("h4").style.color = "hsl(0, 0%, 100%)";
+document.querySelector("p").style.color = "hsla(0, 0%, 100%, 0.75)";
+document.querySelector(".neel").style.color = " hsla(0, 0%, 100%, 0.6)";
+document.querySelector(".reet1").style.color = " hsla(0, 0%, 100%, 0.6)";
+document.querySelector(".letter").style.color = "hsla(0, 0%, 100%, 0.75)";
+document.querySelector(".gaurang").style.color = " hsla(0, 0%, 100%, 0.6)";
